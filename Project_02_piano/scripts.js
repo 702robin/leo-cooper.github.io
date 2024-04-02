45cb9591a2fc3ce0fcc639227ed88776e3950d11
702robin <60797319+702robin@users.noreply.github.com>
2024-04-02 17:52:57 -0400

    files + test

--- Project_02_piano/scripts.js
+++ Project_02_piano/scripts.js
@@ -1,12 +1,28 @@
-function sndFX_click(){
-    var audio = document.createElement("audio");
-    audio.src = "https://www.soundjay.com/buttons/sounds/button-2.mp3";
-    audio.addEventListener("ended", function () {
-        document.removeChild(this);
-    }, false);
-    audio.play();   
-}
-
-$('#makeNoise').bind("click",function() {
-sndFX_click();
-}); 
+const sounds = {
+    'KeyW': 'https://www.virtualdrumming.com/drums/virtual-drum-sounds/hip-hop/tom1.ogg',
+    'KeyA': 'https://www.virtualdrumming.com/drums/virtual-drum-sounds/hip-hop/tom2.ogg',
+    'KeyS': 'https://www.virtualdrumming.com/drums/virtual-drum-sounds/hip-hop/tom3.ogg',
+    'KeyD': 'https://www.virtualdrumming.com/drums/virtual-drum-sounds/hip-hop/pad1.ogg',
+    'KeyJ': 'https://www.virtualdrumming.com/drums/virtual-drum-sounds/hip-hop/snare1.ogg',
+    'KeyK': 'https://www.virtualdrumming.com/drums/virtual-drum-sounds/crash1.ogg',
+    'KeyL': 'https://www.virtualdrumming.com/drums/virtual-drum-sounds/hip-hop/kik.ogg'
+  }
+  
+  const play = sound => {
+    console.log("playing",sound)
+    var audio = new Audio(sound);
+    audio.play();
+  }
+  
+  
+  document.getElementById('drumSet').addEventListener('click', function(e) {
+    const tgt = e.target.closest('button');
+    if (tgt) play(sounds[tgt.id])
+  })
+  
+  window.addEventListener('keypress', function(e) { console.log(e.code)
+    if (sounds[e.code]) {
+      console.log("clicking",e.code)
+      document.getElementById(e.code).click()
+    }  
+  })
