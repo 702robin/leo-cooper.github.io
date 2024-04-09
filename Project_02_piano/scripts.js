3bfb9e6dec2a64101dd1839d03cf288fc54bf88b
702robin <60797319+702robin@users.noreply.github.com>
2024-04-09 15:54:17 -0400

    all keys for winds

--- Project_02_piano/scripts.js
+++ Project_02_piano/scripts.js
@@ -1,6 +1,81 @@
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 65) {
-      document.getElementById('note1').play();
+//need to write array where all of these can be accessed
+//need to write a swtich that accesses different instruments 
+
+document.addEventListener('keydown', function(a) {
+    if (a.keyCode == 65) {
+      document.getElementById('wind1').play();
+    } else {
+      document.addEventListener('keyup', function(a) {})
+    }
+  });
+  document.addEventListener('keydown', function(w) {
+    if (w.keyCode == 87) {
+      document.getElementById('wind2').play();
+    }
+  });
+  document.addEventListener('keydown', function(s) {
+    if (s.keyCode == 83) {
+      document.getElementById('wind3').play();
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 69) {
+      document.getElementById('wind4').play();
+    }
+  });
+  document.addEventListener('keydown', function(d) {
+    if (d.keyCode == 68) {
+      document.getElementById('wind5').play();
+    }
+  });
+  document.addEventListener('keydown', function(f) {
+    if (f.keyCode == 70) {
+      document.getElementById('wind6').play();
+    }
+  });
+  document.addEventListener('keydown', function(t) {
+    if (t.keyCode == 84) {
+      document.getElementById('wind7').play();
+    }
+  });
+  document.addEventListener('keydown', function(g) {
+    if (g.keyCode == 71) {
+      document.getElementById('wind8').play();
+    }
+  });
+  document.addEventListener('keydown', function(y) {
+    if (y.keyCode == 89) {
+      document.getElementById('wind9').play();
+    }
+  });
+  document.addEventListener('keydown', function(h) {
+    if (h.keyCode == 72) {
+      document.getElementById('wind10').play();
+    }
+  });
+  document.addEventListener('keydown', function(j) {
+    if (j.keyCode == 74) {
+      document.getElementById('wind11').play();
+    }
+  });
+  document.addEventListener('keydown', function(i) {
+    if (i.keyCode == 73) {
+      document.getElementById('wind12').play();
+    }
+  });
+  document.addEventListener('keydown', function(k) {
+    if (k.keyCode == 75) {
+      document.getElementById('wind13').play();
+    }
+  });
+  document.addEventListener('keydown', function(o) {
+    if (o.keyCode == 79) {
+      document.getElementById('wind14').play();
+    }
+  });
+  document.addEventListener('keydown', function(l) {
+    if (l.keyCode == 76) {
+      document.getElementById('wind15').play();
     }
   });
 
