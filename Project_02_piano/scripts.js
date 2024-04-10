944f3c115daa807f5b05ed9407ce05a99f8d8272
702robin <60797319+702robin@users.noreply.github.com>
2024-04-10 16:25:39 -0400

    all sound quality settings

--- Project_02_piano/scripts.js
+++ Project_02_piano/scripts.js
@@ -8,82 +8,71 @@
 
 let winds = ["wind1","wind2","wind3","wind4","wind5","wind6","wind7","wind8","wind9","wind10","wind11","wind12","wind13","wind14","wind15"];
 
+function audioVolumeOut(q){
+  if(q.volume){
+     let InT = 1;
+     let setVolume = 0;  
+     let speed = 0.05;
+     q.volume = InT;
+     let fAudio = setInterval(function(){
+         InT -= speed;
+         q.volume = InT.toFixed(1);
+         if(InT.toFixed(1) <= setVolume){
+            q.pause();
+            q.currentTime = 0;
+            q.volume = 1;
+            clearInterval(fAudio);
+         };
+     },1);
+  };
+};
+
+function playAudio(key, id){
+  if (a.keyCode == key) {
+    document.getElementById(id).play();
+  }  
+}
+
+function audioVolumeOut(key, id){
+  if (a.keyCode == key) {
+    document.getElementById(id).audioVolumeOut();
+  }
+}
+
 document.addEventListener('keydown', function(a) {
-    if (a.keyCode == 65) {
-      document.getElementById('wind1').play();
-    } else {
-      document.addEventListener('keyup', function(a) {})
-    }
-  });
-  document.addEventListener('keydown', function(w) {
-    if (w.keyCode == 87) {
-      document.getElementById('wind2').play();
-    }
-  });
-  document.addEventListener('keydown', function(s) {
-    if (s.keyCode == 83) {
-      document.getElementById('wind3').play();
-    }
-  });
-  document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 69) {
-      document.getElementById('wind4').play();
-    }
-  });
-  document.addEventListener('keydown', function(d) {
-    if (d.keyCode == 68) {
-      document.getElementById('wind5').play();
-    }
-  });
-  document.addEventListener('keydown', function(f) {
-    if (f.keyCode == 70) {
-      document.getElementById('wind6').play();
-    }
-  });
-  document.addEventListener('keydown', function(t) {
-    if (t.keyCode == 84) {
-      document.getElementById('wind7').play();
-    }
-  });
-  document.addEventListener('keydown', function(g) {
-    if (g.keyCode == 71) {
-      document.getElementById('wind8').play();
-    }
-  });
-  document.addEventListener('keydown', function(y) {
-    if (y.keyCode == 89) {
-      document.getElementById('wind9').play();
-    }
-  });
-  document.addEventListener('keydown', function(h) {
-    if (h.keyCode == 72) {
-      document.getElementById('wind10').play();
-    }
-  });
-  document.addEventListener('keydown', function(j) {
-    if (j.keyCode == 74) {
-      document.getElementById('wind11').play();
-    }
-  });
-  document.addEventListener('keydown', function(i) {
-    if (i.keyCode == 73) {
-      document.getElementById('wind12').play();
-    }
-  });
-  document.addEventListener('keydown', function(k) {
-    if (k.keyCode == 75) {
-      document.getElementById('wind13').play();
-    }
-  });
-  document.addEventListener('keydown', function(o) {
-    if (o.keyCode == 79) {
-      document.getElementById('wind14').play();
-    }
-  });
-  document.addEventListener('keydown', function(l) {
-    if (l.keyCode == 76) {
-      document.getElementById('wind15').play();
-    }
-  });
+    playAudio(65, 'wind1');
+    playAudio(87, 'wind2');
+    playAudio(83, 'wind3');
+    playAudio(69, 'wind4');
+    playAudio(68, 'wind5');
+    playAudio(70, 'wind6');
+    playAudio(84, 'wind7');
+    playAudio(71, 'wind8');
+    playAudio(89, 'wind9');
+    playAudio(72, 'wind10');
+    playAudio(74, 'wind11');
+    playAudio(73, 'wind12');
+    playAudio(75, 'wind13');
+    playAudio(79, 'wind14');
+    playAudio(76, 'wind15');
+    });
+
+  document.addEventListener('keyup', function(a) {
+    audioVolumeOut(65, 'wind1');
+    audioVolumeOut(87, 'wind2');
+    audioVolumeOut(83, 'wind3');
+    audioVolumeOut(69, 'wind4');
+    audioVolumeOut(68, 'wind5');
+    audioVolumeOut(70, 'wind6');
+    audioVolumeOut(84, 'wind7');
+    audioVolumeOut(71, 'wind8');
+    audioVolumeOut(89, 'wind9');
+    audioVolumeOut(72, 'wind10');
+    audioVolumeOut(74, 'wind11');
+    audioVolumeOut(73, 'wind12');
+    audioVolumeOut(75, 'wind13');
+    audioVolumeOut(79, 'wind14');
+    audioVolumeOut(76, 'wind15');
+    });
 
   
