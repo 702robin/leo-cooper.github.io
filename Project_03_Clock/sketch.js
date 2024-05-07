92c191ef4db4158cd670afa3e029b68d7c91bec3
702robin <60797319+702robin@users.noreply.github.com>
2024-05-07 19:32:45 -0400

    jh;

--- Project_03_Clock/sketch.js
+++ Project_03_Clock/sketch.js
@@ -1,16 +1,112 @@
 let audioPlayer = document.getElementById('audioPlayer');
 
+function updateSongAndArtist(hour) {
+    if (hour === 0) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 1) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 2) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 3) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 4) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 5) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 6) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 7) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 8) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 9) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 10) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 11) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 12) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 13) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 14) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 15) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 16) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 17) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 18) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 19) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 20) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 21) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 22) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+    else if (hour === 23) {
+        document.getElementById('song').innerHTML = "testMIDNIGHT";
+        document.getElementById('artist').innerHTML = "Khruangbin";
+    }
+}
+
+
 function updateTime() {
     let today = new Date();
     let thisHour = today.getHours();
     let song = getSong(thisHour);
     let artist = getArtist(thisHour);
 
-    if (hour === 18){
-    document.getElementById('song').innerHTML = "testMIDNIGHT";
-    document.getElementById('artist').innterHTML = "Khruangbin";
     }
-
     //audioooo
     let audioUrl = getAudioUrl(thisHour);
 
@@ -20,7 +116,6 @@ function updateTime() {
         audioPlayer.play();
     }
 
-}
 
 // Function to get the audio URL based on the hour
 function getAudioUrl(hour) {
